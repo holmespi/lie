4741e1908f7ec8e84510ba5502e0d0efd49a2d5b
holmespi <holmespi90@gmail.com>
2014-02-26 14:15:32 -0500

    updated views for home

--- content/views.py
+++ content/views.py
@@ -3,18 +3,31 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
-
+from content.models import Slider
 
 def home(request):
 	context = RequestContext(request)
 
-	return render_to_response('home.html', context)
+	slider_list = Slider.objects.all()
+	context_dict = {
+	'page_title': 'Love is Earth - Home',
+	'page_desc': 'Love is Earth - Representing the dirty south trap vibe, with relevant streetwear and cultre',
+	'page_type': 'website',
+	'page_image': ''
+	}
+	context_dict['slides'] = slider_list
+	return render_to_response('home.html', context_dict, context)
 
 
 def store(request):
 	context = RequestContext(request)
-
-	return render_to_response('store.html', context)
+	context_dict = {
+	'page_title': 'Love is Earth - Store',
+	'page_desc': 'Love is Earth - Representing the dirty south trap vibe, with relevant streetwear and cultre',
+	'page_type': 'website',
+	'page_image': ''
+	}
+	return render_to_response('store.html', context_dict, context)
 
 
 def deadstock(request):
