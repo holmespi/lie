fd6ff4a07f8357577317b1f6d068ffb8b5f5a0c0
holmespi <holmespi90@gmail.com>
2014-02-25 19:53:11 -0500

    Started the views for the each page

--- content/views.py
+++ content/views.py
@@ -6,23 +6,38 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 
 def home(request):
-	context = RequestContext(request)	
+	context = RequestContext(request)
+
+	return render_to_response('home.html', context)
+
 
 def store(request):
 	context = RequestContext(request)
 
+	return render_to_response('store.html', context)
+
+
 def deadstock(request):
 	context = RequestContext(request)
+	
+	return render_to_response('deadstock.html', context)
 
 def news(request):
 	context = RequestContext(request)
 
+	return render_to_response('news.html', context)
+
 def contact(request):
 	context = RequestContext(request)
 
-def collection(request):
+	return render_to_response('contact.html', context)
+
+def collections(request):
 	context = RequestContext(request)
 
-def stockist(request):
+	return render_to_response('collections.html', context)
+
+def stockists(request):
 	context = RequestContext(request)
 
+	return render_to_response('stockists.html', context)
