9bdb7129bfc7a9329190cabefc6c342b2869e94c
holmespi <holmespi90@gmail.com>
2014-02-26 14:14:59 -0500

    updated models to include slider image field

--- content/models.py
+++ content/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
-
 # Create your models here.
+
+class Slider(models.Model):
+	test = models.CharField(max_length=128, null=True, blank=True)
+	images = models.FileField(upload_to="slider_images")
 
 class Categories(models.Model):
     category = models.CharField(max_length=30)
@@ -16,6 +19,7 @@ class Post(models.Model):
 	title = models.CharField(max_length=128, unique=True)
 	date = models.DateField(auto_now=True)
 	description = models.TextField()
+	photo = models.FileField(upload_to='news') #change this to a URLField for production or else find a way to manage ftp uploads from django admin
 
 	def __unicode__(self):
 		return self.title
@@ -24,11 +28,12 @@ class Post(models.Model):
 
 class Product(models.Model):
 	stock = models.BooleanField()
+	thumb = models.FileField(upload_to='products') #change this to a URLField for production or else find a way to manage ftp uploads from django admin 
 	name = models.CharField(max_length=128, unique=True)
-	price = models.IntegerField()
+	price = models.DecimalField(max_digits=6, decimal_places=2)
 	description = models.TextField()
 	category = models.ManyToManyField(Categories)
-	size = models.CharField(max_length=128, unique=True)
+	size = models.CharField(max_length=128)
 
 	def __unicode__(self):
 		return self.name
@@ -46,6 +51,7 @@ class Stockist(models.Model):
 
 class Collection(models.Model):
 	name = models.CharField(max_length=128)
+	photo = models.FileField(upload_to='collections') #change this to a URLField for production or else find a way to manage ftp uploads from django admin
 	link = models.URLField(max_length=200)
 	products = models.ManyToManyField(Product)
 
